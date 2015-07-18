0adf1c43800679889b84a996e4eb121bc6ada331
Abhay Kumar <aksonuabhay@gmail.com>
2015-07-18 14:27:51 +0530

    UDP bidirectional communication implementation test

--- Network/UDP/comm.udp.client/src/main/java/comm/udp/client/CommUdpClientActivity.java
+++ Network/UDP/comm.udp.client/src/main/java/comm/udp/client/CommUdpClientActivity.java
@@ -4,6 +4,11 @@ import interactivespaces.activity.impl.BaseActivity;
 import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpoint;
 import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpointListener;
 import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpointService;
+import interactivespaces.service.comm.network.server.UdpServerNetworkCommunicationEndpoint;
+import interactivespaces.service.comm.network.server.UdpServerNetworkCommunicationEndpointListener;
+import interactivespaces.service.comm.network.server.UdpServerNetworkCommunicationEndpointService;
+import interactivespaces.service.comm.network.server.UdpServerRequest;
+
 import java.net.InetSocketAddress;
 
 /**
@@ -14,12 +19,13 @@ public class CommUdpClientActivity extends BaseActivity {
 	private UdpClientNetworkCommunicationEndpoint udpClient;
 	private InetSocketAddress udpServerAddress;
 	
+	UdpServerNetworkCommunicationEndpoint udpServer;
 	
     @Override
     public void onActivitySetup() {
         getLog().info("Activity comm.udp.client setup");
         UdpClientNetworkCommunicationEndpointService udpClientService = getSpaceEnvironment().getServiceRegistry().getRequiredService(UdpClientNetworkCommunicationEndpointService.SERVICE_NAME);
-        String UdpServerHost = getConfiguration().getPropertyString("space.comm.udp.server.host","127.0.0.1");
+        String UdpServerHost = getConfiguration().getPropertyString("space.comm.udp.server.host","192.168.107.128");
         int udpServerPort =getConfiguration().getRequiredPropertyInteger("space.comm.udp.server.port");
         udpClient = udpClientService.newClient(getLog());
         udpClient.addListener(new UdpClientNetworkCommunicationEndpointListener() {
@@ -27,12 +33,26 @@ public class CommUdpClientActivity extends BaseActivity {
 			public void onUdpResponse(UdpClientNetworkCommunicationEndpoint client,
 					byte[] data, InetSocketAddress address) {
 				String message = new String(data);
-				getLog().info("From : "+ address.toString()+" Client received : " + message);
+				getLog().info("Client : "+"From : "+ address.toString()+" Client received : " + message);
 				
 			}
 		});
         addManagedResource(udpClient);
         udpServerAddress = new InetSocketAddress(UdpServerHost, udpServerPort);
+        
+        UdpServerNetworkCommunicationEndpointService udpServerService = getSpaceEnvironment().getServiceRegistry().getRequiredService(UdpServerNetworkCommunicationEndpointService.SERVICE_NAME);
+        
+        udpServer = udpServerService.newServer(udpServerPort, getLog());
+        udpServer.addListener(new UdpServerNetworkCommunicationEndpointListener() {
+			
+			public void onUdpRequest(UdpServerNetworkCommunicationEndpoint server,
+					UdpServerRequest req) {
+				String message = new String(req.getRequest());
+				getLog().info(req.getRemoteAddress() + "<- client sent server -> " +message);
+				req.writeResponse("Client : Server recieved your message and is replying".getBytes());
+			}
+		});
+        addManagedResource(udpServer);
     }
 
     @Override
--- Network/UDP/comm.udp.server/src/main/java/comm/udp/server/CommUdpServerActivity.java
+++ Network/UDP/comm.udp.server/src/main/java/comm/udp/server/CommUdpServerActivity.java
@@ -1,6 +1,11 @@
 package comm.udp.server;
 
+import java.net.InetSocketAddress;
+
 import interactivespaces.activity.impl.BaseActivity;
+import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpoint;
+import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpointListener;
+import interactivespaces.service.comm.network.client.UdpClientNetworkCommunicationEndpointService;
 import interactivespaces.service.comm.network.server.UdpServerNetworkCommunicationEndpoint;
 import interactivespaces.service.comm.network.server.UdpServerNetworkCommunicationEndpointListener; 
 import interactivespaces.service.comm.network.server.UdpServerNetworkCommunicationEndpointService;
@@ -12,6 +17,9 @@ import interactivespaces.service.comm.network.server.UdpServerRequest;
 public class CommUdpServerActivity extends BaseActivity {
 
 	UdpServerNetworkCommunicationEndpoint udpServer;
+	
+	private UdpClientNetworkCommunicationEndpoint udpClient;
+	private InetSocketAddress udpServerAddress;
     @Override
     public void onActivitySetup() {
         getLog().info("Activity comm.udp.server setup");
@@ -24,10 +32,26 @@ public class CommUdpServerActivity extends BaseActivity {
 					UdpServerRequest req) {
 				String message = new String(req.getRequest());
 				getLog().info(req.getRemoteAddress() + "<- client sent server -> " +message);
-				req.writeResponse("Server recieved your message and is replying".getBytes());
+				req.writeResponse("SERVER : Server recieved your message and is replying".getBytes());
 			}
 		});
         addManagedResource(udpServer);
+        
+        UdpClientNetworkCommunicationEndpointService udpClientService = getSpaceEnvironment().getServiceRegistry().getRequiredService(UdpClientNetworkCommunicationEndpointService.SERVICE_NAME);
+        String UdpServerHost = getConfiguration().getPropertyString("space.comm.udp.server.host","192.168.107.1");
+        int udpServerPort =getConfiguration().getRequiredPropertyInteger("space.comm.udp.server.port");
+        udpClient = udpClientService.newClient(getLog());
+        udpClient.addListener(new UdpClientNetworkCommunicationEndpointListener() {
+			
+			public void onUdpResponse(UdpClientNetworkCommunicationEndpoint client,
+					byte[] data, InetSocketAddress address) {
+				String message = new String(data);
+				getLog().info("SERVER : Client : "+"From : "+ address.toString()+" Client received : " + message);
+				
+			}
+		});
+        addManagedResource(udpClient);
+        udpServerAddress = new InetSocketAddress(UdpServerHost, udpServerPort);
     }
 
     @Override
@@ -43,11 +67,13 @@ public class CommUdpServerActivity extends BaseActivity {
     @Override
     public void onActivityActivate() {
         getLog().info("Activity comm.udp.server activate");
+        udpClient.write(udpServerAddress, "SERVER Client activated".getBytes());
     }
 
     @Override
     public void onActivityDeactivate() {
         getLog().info("Activity comm.udp.server deactivate");
+        udpClient.write(udpServerAddress, "SERVER Client deactivated".getBytes());
     }
 
     @Override
